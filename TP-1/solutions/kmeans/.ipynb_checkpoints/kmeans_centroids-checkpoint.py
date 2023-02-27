83f853f487d360b90f298e68eb23dd6f7f5ca6c2
Juliette Chevallier <juliette.chevallier@insa-toulouse.fr>
2023-02-27 12:07:18 +0100

    link TP1-2

--- TP-1/solutions/kmeans/.ipynb_checkpoints/kmeans_centroids-checkpoint.py
+++
@@ -1,9 +0,0 @@
-K = 6
-
-x = np.arange(0, 255)
-for i in range(K):
-    plt.plot(x, np.mean(mars_data[clusters_pca == i], axis = 0), color=cmap.colors[i])
-    
-plt.xlabel("Wave length")
-plt.ylabel("Measured value")
-plt.show()
